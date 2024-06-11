bae1b72a94755d52e7cecf1e3915bc15f9ba6a9b
zoraaga1 <fati.sousse12@gmail.com>
2024-06-11 01:39:46 +0100

    all tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,9 +10,9 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """returns the list of all the delays"""
     delays: List[float] = [wait_random(max_delay) for _ in range(n)]
     completed_delays: List[float] = []
-    
+
     for delay in asyncio.as_completed(delays):
         result = await delay
         completed_delays.append(result)
-        
+
     return completed_delays
